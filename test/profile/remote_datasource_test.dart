dd33f6aaeb87e2b94800866c849aeb28c64898d0
nabildzr <muhammadnbillz@gmail.com>
2025-07-18 00:00:00 +0700

    add test method getAllUser()

--- test/profile/remote_datasource_test.dart
+++ test/profile/remote_datasource_test.dart
@@ -12,6 +12,7 @@ void main() async {
   var remoteDataSource = MockProfileRemoteDataSoure(); // fake class
 
   const int userId = 1;
+  const int page = 1;
 
   final ProfileModel fakeProfileModel = ProfileModel(
     id: userId,
@@ -46,6 +47,38 @@ void main() async {
 
         try {
           var response = await remoteDataSource.getUserById(1);
+          fail("TIDAK MUNGKIN TERJADI - ERROR");
+        } catch (e) {
+          // PASTI BERHASIL
+          expect(e, isException);
+        }
+      });
+    });
+
+    group("getAllUser()", () {
+      test('BERHASIL (200)', () async {
+        //  stub -> kondisi untuk mempalsukan
+        // (proses stubbing)
+        when(
+          remoteDataSource.getAllUser(1),
+        ).thenAnswer((_) async => [fakeProfileModel]);
+
+        try {
+          // PASTI BERHASIL
+          var response = await remoteDataSource.getAllUser(1);
+          expect(response, [fakeProfileModel]);
+        } catch (e) {
+          fail("TIDAK MUNGKIN TERJADI - ERROR");
+        }
+      });
+
+      test('GAGAL', () async {
+        //  stub -> kondisi untuk mempalsukan
+        // (proses stubbing)
+        when(remoteDataSource.getAllUser(1)).thenThrow(Exception());
+
+        try {
+          var response = await remoteDataSource.getAllUser(1);
           fail("TIDAK MUNGKIN TERJADI - ERROR");
         } catch (e) {
           // PASTI BERHASIL
